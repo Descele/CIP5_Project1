586383d47931599afa2a02bea2290f112d48b10a
DesireeC <nosipho.work@gmail.com>
2025-05-29 13:21:12 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 """
-Hello World! Desiree Cele created a Buffalo-Cheektowaga Housing Data Explorer: 
-This program analyzes and displays a real-world housing statistics sample from S2501 OCCUPANCY CHARACTERISTICS Buffalo-Cheektowaga, NY metro area. 
+Desiree Cele created this Housing Data Explorer: 
+This program analyzes and displays from real-world housing statistics sample of S2501 OCCUPANCY CHARACTERISTICS Buffalo-Cheektowaga, NY metro area. 
 It allows users to extract key data points(raw data), exploring occupancy rates, household sizes, and housing types with margin-of-error data. 
 The tool reads from a CSV dataset, analyses housing records in seconds and provides an interactive menu for simple data exploration.
 """
